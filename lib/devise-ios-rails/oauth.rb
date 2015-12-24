c2720f9557a41ebf55031cf316e0ea75f37e8ee6
Roman Krayovskyy <rkrayovskyy@gmail.com>
2015-12-24 10:59:18 +0200

    Update oauth.rb
    extend user profile data to be updated from FB

--- lib/devise-ios-rails/oauth.rb
+++ lib/devise-ios-rails/oauth.rb
@@ -18,6 +18,15 @@ module DeviseIosRails
     module ClassMethods
       def from_oauth attributes
         where(attributes.slice(:uid, :provider)).first_or_create do |user|
+          user.email       = attributes[:email]
+          user.first_name  = attributes[:first_name]   # assuming the user model has a name
+      		user.last_name   = attributes[:last_name]
+      		user.avatar      = attributes[:avatar] # assuming the user model has an image
+          user.gender      = attributes[:gender]
+          user.birthday    = attributes[:birthday]
+          user.country     = attributes[:country]
+          user.state       = attributes[:state]
+          user.city        = attributes[:city]
           user.oauth_email = attributes[:email]
           user.provider    = attributes[:provider]
           user.uid         = attributes[:uid]
